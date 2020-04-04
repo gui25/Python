bbc35beb8412456f0aabcd454443a28d6c038428
Guilherme <gui25ber@gmail.com>
2020-04-03 22:02:36 -0300

    mudei classe pra text e n char

--- game.py
+++ game.py
@@ -2,7 +2,7 @@ import sqlite3
 
 conn = sqlite3.connect('personagem.db')
 
-conn.execute('create table IF NOT EXISTS Status(id integer primary key autoincrement, nome text, xp integer, damage integer, life integer,classe char)')
+conn.execute('create table IF NOT EXISTS Status(id integer primary key autoincrement, nome text, xp integer, damage integer, life integer,classe text)')
 
 option = 0
 while option >= 0:
@@ -13,11 +13,22 @@ while option >= 0:
         print('saindo...')
 
     if option == 1:
+        flag=0
         name = input('\nDigite o seu nome:')
         xp = 10
         damage = 15
         life = 30
-        classe = input('\nDigite:\nM para ser um mago\nG para um Guerreiro\nL para ser um ladrão ')
+        while flag != 5:
+            classe = input('\nDigite:\nM para ser um mago\nG para um Guerreiro\nL para ser um ladrão ')
+            if classe == 'm' or classe == 'M':
+                classe = 'Mago'
+                flag = 5
+            if classe == 'g' or classe == 'G':
+                classe = 'Guerreiro'
+                flag = 5
+            if classe == 'L' or classe == 'l':
+                classe = 'Mago'
+                flag = 5
         conn.execute('insert into Status (nome,xp,damage,life,classe) values (?,?,?,?,?)',(name,xp,damage,life,classe))
         conn.commit()
         
