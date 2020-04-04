c4ed477e533dabae8bd58af78180e7e1c12fafe7
MatheusOliver333 <60905505+MatheusOliver333@users.noreply.github.com>
2020-04-04 14:44:25 -0300

    Title
    Coloquei uma frase bonitinha para os usuarios

--- game.py
+++ game.py
@@ -6,6 +6,7 @@ conn.execute('create table IF NOT EXISTS Status(id integer primary key autoincre
 
 option = 0
 while option >= 0:
+    print(""" Bem vindo ao nosso maravilhoso game!!!\n Neste jogo de RPG no seu terminal você irá\n Primeiramente criar seu personagem, por enquanto há poucas opções de char\n Mas logo logo com nossas atualizações você irá vivenciar um jogo incrivel!!\n E sim, um jogo de terminal(cmd)\n Bom Jogo!!""")
     option = int(input('\nEscolha a opção:\n0 para sair\n1 para criar um personagem\n2 para escolher um personagem criado\n'))
     
     if option == 0:
@@ -17,7 +18,7 @@ while option >= 0:
         xp = 10
         damage = 15
         life = 30
-        classe = input('\nDigite:\nM para ser um mago\nG para um Guerreiro\nL para ser um ladrão ')
+        classe = input('\nDigite:\nM para ser um mago\nG para um Guerreiro\nL para ser um ladrão')
         conn.execute('insert into Status (nome,xp,damage,life,classe) values (?,?,?,?,?)',(name,xp,damage,life,classe))
         conn.commit()
         
