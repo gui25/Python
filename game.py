f836534202452e0be07b5b5a39528b66e5b615c0
Guilherme <gui25ber@gmail.com>
2020-04-04 04:23:52 -0300

    UPDATE

--- game.py
+++ game.py
@@ -50,13 +50,70 @@ while option >= 0:
         for row in conn.execute('select * from Personagem'):
             ID,nome,xp,gold,damage,life,classe = row
             print('| O ID é:', ID, ' | O NOME é:', nome, ' | O DANO é: ', damage, ' | QUANTIDADE DE OURO: ', gold, ' | A VIDA é: ', life, ' | A CLASSE é ', classe,' | ')
-
-        selec = input('Selecione pelo ID o personagem que deseja jogar')
+        print("\n")
+        selec = input('Selecione pelo ID o personagem que deseja jogar: ')
         for row in conn.execute('select * from Personagem where ID = ?',(selec,)):
             ID,nome,xp,gold,damage,life,classe = row
             print('\nVocê selecionou o ',classe,' conhecido como ',nome)
-            jogo(ID,nome,xp,gold,damage,life,classe)
+            
+            escolha = 0
+            while escolha >= 0:
+                run = 0
+                atuallife = life
+                atualxp = xp
+                atualdamage = damage
+                atualgold = gold
 
+                escolha = int(input('\nO que você deseja fazer:\n0 - ir para o menu\n1 - Explorar caverna\n'))
+
+                if escolha == 0:
+                    escolha = -1
+                    print('\nsaindo...')
+
+                if escolha == 1:
+                    #rand = randint(0,9) quando tiver 9 possibilidades eu ativo
+                    rand = 1
+
+                    if rand == 1:
+
+                        print('\nVocê encontrou com um goblin!')
+                        enemylife = 13
+                        enemydamage = 10
+                        print('\nEle não quis nem conversar e uma batalha começou:')
+                        print(atuallife)
+                        print(enemylife)
+                        print(run)
+
+                        while enemylife >= 0 and atuallife >= 0 and run <= 8:
+                            run = 0
+                            print('\nTurno do goblin:')
+                            damagegiven = randint(0,enemydamage)
+                            print('\nVocê levou ',damagegiven,' de dano')
+                            atuallife = atuallife - damagegiven
+                            print('\nStatus (',nome,'): vida atual',atuallife)
+                            if atuallife >= 0:
+                                option = int(input('1 pra  fugir ou 2 pra lutar:'))
+
+                                if option == 1:
+                                    run = randint(0,10)
+                                    if(run >= 8):
+                                        print('\n Você fugiu com sucesso!')
+                                elif option == 2:
+                                    print('\nSeu turno:')
+                                    damagegiven = randint(0,atualdamage)
+                                    print('\nVocê deu ',damagegiven,' de dano')
+                                    print('\nSeu inimigo está com ',enemylife,' de vida')
+                                else:
+                                    print('\nVocê ficou sem reação, não ira lutar dessa vez.')
+                        else:
+                            if enemylife <= 0:
+                                    winxp = randint(30,60)
+                                    print('Você ganhou ',winxp,'xp.\nParabéns!')
+                            if atuallife <= 0:
+                                    lostgold = randint(0,atualgold)
+                                    atualgold = atualgold - lostgold
+                                    print('Um curandeiro encontrou seu corpo e o trouxe de volta.')
+                                    print('Porém quando morto você perdeu ',lostgold,' de ouro')
 
     if option == 3:
         print("\n")
@@ -67,8 +124,8 @@ while option >= 0:
         IdExluir = int(input('\nDigite o ID do personagem que deseja excluir: '))
         confirm = 0
         for row in conn.execute('select * from Personagem'):
-            ID,nome,xp,gold,damage,life,classe = row
-            if(ID==IdExluir):
+             ID,nome,xp,gold,damage,life,classe = row
+             if(ID==IdExluir):
                 print('\nPersonagem excluido com sucesso!')
                 confirm = 5
                 conn.execute('delete from Personagem where ID = ?',(IdExluir,))
@@ -77,50 +134,7 @@ while option >= 0:
             print('\nID não encontrado.')
 
 
-def jogo(ID,nome,xp,gold,damage,life,classe):
-    atuallife = life
-    atualxp = xp
-    atualdamage = damage
-    atualgold = gold
-    run = 0
-    escolha = ('\n O que você deseja fazer:\n1 - Explorar caverna\n')
-    if escolha == 1:
-        rand = randint(0,9)
-        if rand == 1:
 
-            print('\nVocê encontrou com um goblin!')
-            enemylife = 13
-            enemydamage = 10
-            run = 0
-            print('\nEle não quis nem conversar e uma batalha começou:')
-
-            while enemylife != 0 or atuallife != 0 or run >= 8:
-                print('\nTurno do goblin:')
-                damagegiven = randint(0,enemydamage)
-                print('\nVocê levou ',damagegiven,' de dano')
-                atuallife = atuallife - damagegiven
-                print('\nStatus (',nome,'): vida atual',atuallife)
-                option = int(input('1 pra  fugir ou 2 pra lutar:'))
-                if option == 1:
-                    run = randint(0,10)
-                    if(run >= 8):
-                        print('\n Você fugiu com sucesso!')
-                elif option == 2:
-                    print('\nSeu turno:')
-                    damagegiven = randint(0,atualdamage)
-                    print('\nVocê deu ',damagegiven,' de dano')
-                    print('\nSeu inimigo está com ',enemylife,' de vida')
-                else:
-                    print('\nVocê ficou sem reação, não ira lutar dessa vez.')
-            else:
-                if enemylife == 0:
-                    winxp = randint(30,60)
-                    print('Você ganhou ',winxp,'xp.\nParabéns!')
-                if atuallife == 0:
-                    lostgold = randint(0,atualgold)
-                    atualgold = atualgold - lostgold
-                    print('Um curandeiro encontrou seu corpo e o trouxe de volta.')
-                    print('Porém quando morto você perdeu ',lostgold,' de ouro')
                     
                 
                     
